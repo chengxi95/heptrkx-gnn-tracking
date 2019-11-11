86629d05a1e97df341708d12e8aeccec84fe4c73
Xi <chengxi95@qq.com>
2019-11-11 17:05:52 -0500

    change binary cross entroy with logit to binary cross entroy, add more debugging information, add config for larger dataset

--- trainers/gnn_sparse.py
+++ trainers/gnn_sparse.py
@@ -22,18 +22,30 @@ class SparseGNNTrainer(GNNBaseTrainer):
         # Prepare summary information
         summary = dict()
         sum_loss = 0
+        sum_correct = 0
+        sum_total = 0
 
         # Loop over training batches
         for i, batch in enumerate(data_loader):
             batch = batch.to(self.device)
             self.model.zero_grad()
             batch_output = self.model(batch)
-            batch_loss = self.loss_func(batch_output, batch.y.float(), weight=batch.w)
+            batch_pred = torch.sigmoid(batch_output)
+            
+            matches = ((batch_pred > 0.5) == (batch.y > 0.5))
+            sum_correct += matches.sum().item()
+            sum_total += matches.numel()
+
+            batch_loss = self.loss_func(torch.sigmoid(batch_output), batch.y.float().float(), weight=batch.w.float())
 
             batch_loss.backward()
             self.optimizer.step()
             sum_loss += batch_loss.item()
-
+            predict_noise = batch_pred > 0.5
+            predict_hits = batch_pred < 0.5
+            true_noise = batch.y == 1
+            true_hits = batch.y == 0
+            self.logger.debug(f'\n--train batch predict noise: {predict_noise.sum().item()} true hits: {predict_hits.sum().item()} \n--train batch ground  noise: {true_noise.sum().item()} true hits: {true_hits.sum().item()}')
             # Dump additional debugging information
             if self.logger.isEnabledFor(logging.DEBUG):
                 l1 = get_weight_norm(self.model, 1)
@@ -46,12 +58,13 @@ class SparseGNNTrainer(GNNBaseTrainer):
         n_batches = i + 1
         summary['lr'] = self.optimizer.param_groups[0]['lr']
         summary['train_loss'] = sum_loss / n_batches
+        summary['train_acc'] = sum_correct / sum_total
         summary['l1'] = get_weight_norm(self.model, 1)
         summary['l2'] = get_weight_norm(self.model, 2)
         self.logger.debug(' Processed %i batches', n_batches)
         self.logger.debug(' Model LR %f l1 %.2f l2 %.2f',
                           summary['lr'], summary['l1'], summary['l2'])
-        self.logger.info('  Training loss: %.3f', summary['train_loss'])
+        self.logger.info('  Training loss: %.3f acc: %.3f', summary['train_loss'], summary['train_acc'])
         return summary
 
     @torch.no_grad()
@@ -71,11 +84,16 @@ class SparseGNNTrainer(GNNBaseTrainer):
 
             # Make predictions on this batch
             batch_output = self.model(batch)
-            batch_loss = self.loss_func(batch_output, batch.y.float()).item()
+            batch_loss = self.loss_func(torch.sigmoid(batch_output), batch.y.float()).item()
             sum_loss += batch_loss
 
             # Count number of correct predictions
             batch_pred = torch.sigmoid(batch_output)
+            predict_noise = batch_pred > 0.5
+            predict_hits = batch_pred < 0.5
+            true_noise = batch.y == 1
+            true_hits = batch.y == 0
+            self.logger.debug(f'\n--val batch predict noise: {predict_noise.sum().item()} true hits: {predict_hits.sum().item()} \n--val batch ground  noise: {true_noise.sum().item()} true hits: {true_hits.sum().item()}')
             matches = ((batch_pred > 0.5) == (batch.y > 0.5))
             sum_correct += matches.sum().item()
             sum_total += matches.numel()

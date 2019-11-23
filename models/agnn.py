98f3aba0115476a9826129ae55cc11575d1800b8
Xi <chengxi95@qq.com>
2019-11-23 16:47:40 -0500

    change training epoch, add more layers to the output mlp

--- models/agnn.py
+++ models/agnn.py
@@ -87,10 +87,10 @@ class GNNSegmentClassifier(nn.Module):
         
         # Setup the output layers
         #self.output_network = make_mlp(hidden_dim, [hidden_dim, hidden_dim, hidden_dim, 1], output_activation=hidden_activation, layer_norm=layer_norm)
-        self.output_network = nn.Sequential(#nn.Linear(hidden_dim, hidden_dim),
-                                            #nn.ReLU(),
-                                            #nn.Linear(hidden_dim, hidden_dim),
-                                            #nn.ReLU(),
+        self.output_network = nn.Sequential(nn.Linear(hidden_dim, hidden_dim),
+                                            nn.ReLU(),
+                                            nn.Linear(hidden_dim, hidden_dim),
+                                            nn.ReLU(),
                                             nn.Linear(hidden_dim, hidden_dim),
                                             nn.ReLU(),
                                             nn.Linear(hidden_dim, 1))
